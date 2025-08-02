3f0fe2fdc76faa6649b833da1649072d77309789
mazode <mrmaazkhan191@gmail.com>
2025-08-02 19:30:34 +0300

    adjusted the directories and installed dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,19 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div className="">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 export default App;
