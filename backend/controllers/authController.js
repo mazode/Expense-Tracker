5f1b3b373fd63436740e161f6cbfaacf0065866f
mazode <mrmaazkhan191@gmail.com>
2025-08-05 22:37:56 +0300

    add: registerUser auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,6 +5,39 @@ const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 
-exports.registerUser = async (req, res) => {};
+exports.registerUser = async (req, res) => {
+  const { fullName, email, password, imageUrl } = req.body;
+
+  // Check for missing fields
+  if (!fullName || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    // Check if email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    // Create user
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+      imageUrl,
+    });
+
+    res.status(201).json({
+      id: user.id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+  }
+};
 exports.loginUser = async (req, res) => {};
 exports.getUserInfo = async (req, res) => {};
