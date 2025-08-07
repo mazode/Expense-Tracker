1c54da33957049380e8bcf503a2fe56fd077657b
mazode <mrmaazkhan191@gmail.com>
2025-08-07 23:17:26 +0300

    add: setup income controllers functions

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,14 @@
+const User = require("../models/User");
+const Income = require("../models/Income");
+
+// Add Income
+exports.addIncome = async (req, res) => {};
+
+// Delete Income
+exports.deleteIncome = async (req, res) => {};
+
+// Get Income
+exports.getIncome = async (req, res) => {};
+
+// Download Income
+exports.downloadIncome = async (req, res) => {};
