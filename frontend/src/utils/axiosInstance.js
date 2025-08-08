f3b508baaa2d49cd039970587dfb84bb3b8e6044
mazode <mrmaazkhan191@gmail.com>
2025-08-09 00:48:08 +0300

    add: axios instance

---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPaths";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  timeout: 10000,
+});
+
+// Request Interceptor: Add token header before every request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor: Handle common errors globally
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Redirect to login page
+      window.location.href = "/login";
+    } else if (error.response.status === 500) {
+      console.log("Server error. Please try again later");
+    } else if (error.code === "ECONNABORATED") {
+      console.log("Request timeout. Please try again");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
